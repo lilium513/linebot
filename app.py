0d0e89a7006259d841864f847561144a659faf0d
lilium <lim.intefx@gmail.com>
2017-12-20 19:00:45 +0900

    carousel test

--- app.py
+++ app.py
@@ -16,18 +16,69 @@ channel_access_token = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 app = Flask(__name__)
+#ここらへんでルーティングする
 
 @app.route("/")
 def hello():
     return "hello world!"
 
 @app.route("/", methods=['POST'])
-def hello_world():
+def routing():
     body = request.get_data(as_text=True)
     receive_json = json.loads(body)
     print(receive_json)
-    response = receive_json['events'][0]['message']['text']
-    line_bot_api.reply_message(receive_json['events'][0]['replyToken'], TextSendMessage(text = response))
+    route = receive_json['events'][0]['message']['text']
+    carousel_template_message = TemplateSendMessage(
+    alt_text='Carousel template',
+    template=CarouselTemplate(
+        columns=[
+            CarouselColumn(
+                thumbnail_image_url='https://pbs.twimg.com/profile_images/926053940449263616/1I1Q_Plx_400x400.jpg',
+                title='this is menu1',
+                text='description1',
+                actions=[
+                    PostbackTemplateAction(
+                        label='postback1',
+                        text='postback text1',
+                        data='action=buy&itemid=1'
+                    ),
+                    MessageTemplateAction(
+                        label='message1',
+                        text='message text1'
+                    ),
+                    URITemplateAction(
+                        label='uri1',
+                        uri='https://pbs.twimg.com/profile_images/926053940449263616/1I1Q_Plx_400x400.jpg'
+                    )
+                ]
+            ),
+            CarouselColumn(
+                thumbnail_image_url='https://pbs.twimg.com/profile_images/926053940449263616/1I1Q_Plx_400x400.jpg',
+                title='this is menu2',
+                text='description2',
+                actions=[
+                    PostbackTemplateAction(
+                        label='postback2',
+                        text='postback text2',
+                        data='action=buy&itemid=2'
+                    ),
+                    MessageTemplateAction(
+                        label='message2',
+                        text='message text2'
+                    ),
+                    URITemplateAction(
+                        label='uri2',
+                        uri='https://pbs.twimg.com/profile_images/926053940449263616/1I1Q_Plx_400x400.jpg'
+                    )
+                ]
+            )
+        ]
+    )
+)
+
+
+    if "ゲーム" in route:
+        line_bot_api.reply_message(receive_json['events'][0]['replyToken'], template)
     # line_bot_api.reply_message(
     #     event.reply_token,
     return body
